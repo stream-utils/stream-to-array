a3865c6b7284897484e5178cf0f6e8d75c3438f9
Jonathan Ong <jonathanrichardong@gmail.com>
2013-11-09 13:42:47 -0800

    thunk support

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 exports.array = toArray
 exports.buffer = toBuffer
 
-function toArray(stream, callback) {
+function toArray(stream, done) {
   var arr = []
 
   stream.on('data', onData)
   stream.once('end', onEnd)
-  stream.once('error', callback)
+  stream.once('error', onEnd)
   stream.once('error', cleanup)
   stream.once('close', cleanup)
 
@@ -14,30 +14,33 @@ function toArray(stream, callback) {
     arr.push(doc)
   }
 
-  function onEnd() {
-    callback(null, arr)
-    cleanup()
+  function onEnd(err) {
+    process.nextTick(function () {
+      done(err, arr)
+      cleanup()
+    })
   }
 
   function cleanup() {
     arr = null
     stream.removeListener('data', onData)
     stream.removeListener('end', onEnd)
-    stream.removeListener('error', callback)
+    stream.removeListener('error', onEnd)
     stream.removeListener('error', cleanup)
     stream.removeListener('close', cleanup)
   }
 
-  return stream
+  return function (fn) {
+    done = fn
+  }
 }
 
-function toBuffer(stream, callback) {
+function toBuffer(stream, done) {
   toArray(stream, function (err, arr) {
-    if (err || !arr)
-      callback(err)
-    else
-      callback(null, Buffer.concat(arr))
+    done(err, !err && Buffer.concat(arr))
   })
 
-  return stream
+  return function (fn) {
+    done = fn
+  }
 }
