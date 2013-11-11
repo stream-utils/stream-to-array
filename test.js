22bd19bb11406424c0d69ddfc55739b4891813e6
Jonathan Ong <jonathanrichardong@gmail.com>
2013-11-10 21:22:03 -0800

    tests as a yieldable

--- test.js
+++ test.js
@@ -1,10 +1,20 @@
 var assert = require('assert')
 var path = require('path')
 var fs = require('fs')
+var stream = require('stream')
+var co = require('co')
 
 var streamTo = require('./')
 
 var file = path.join(__dirname, 'index.js')
+
+function emptyStream() {
+  var s = new stream()
+  process.nextTick(function () {
+    s.emit('end')
+  })
+  return s
+}
 
 describe('Stream To', function () {
   describe('Array', function () {
@@ -19,19 +29,51 @@ describe('Stream To', function () {
         done()
       })
     })
+
+    it('should work as a yieldable', function (done) {
+      co(function* () {
+        var arr = yield streamTo.array(fs.createReadStream(file))
+        assert.ok(Array.isArray(arr))
+        assert.ok(arr.length)
+      })(done)
+    })
+
+    it('should work as a yieldable with zalgo', function (done) {
+      co(function* () {
+        var arr = yield streamTo.array(emptyStream())
+        assert.ok(Array.isArray(arr))
+        assert.equal(arr.length, 0)
+      })(done)
+    })
   })
 
   describe('Buffer', function () {
     it('should work', function (done) {
-      streamTo.buffer(fs.createReadStream(file), function (err, buffer) {
+      streamTo.buffer(fs.createReadStream(file), function (err, buf) {
         if (err)
           return done(err)
 
-        assert.ok(Buffer.isBuffer(buffer))
-        assert.ok(buffer.length)
+        assert.ok(Buffer.isBuffer(buf))
+        assert.ok(buf.length)
 
         done()
       })
+    })
+
+    it('should work as a yieldable', function (done) {
+      co(function* () {
+        var buf = yield streamTo.buffer(fs.createReadStream(file))
+        assert.ok(Buffer.isBuffer(buf))
+        assert.ok(buf.length)
+      })(done)
+    })
+
+    it('should work as a yieldable with zalgo', function (done) {
+      co(function* () {
+        var buf = yield streamTo.buffer(emptyStream())
+        assert.ok(Buffer.isBuffer(buf))
+        assert.equal(buf.length, 0)
+      })(done)
     })
   })
 })
